aede527fa4e30e047ae58e5ee84123dc62d291c7
Amirul Wiramuda <amirulwiramuda@gmail.com>
2018-12-04 19:51:25 +0700

    pengelompokan hewan

--- pengelompokanhewan.js
+++ pengelompokanhewan.js
@@ -1,25 +1,23 @@
 function groupAnimals (animals) {
   // you can only write your code here!
-
-  var kandang = [[]]
-  var result = []
-  var kelompokKandang = []
-  for (var i = 0; i <= animals.length - 1; i++) {
-    kandang.push(animals[i])
-    if (animals[i][0] === kandang[i][0]) {
-      kandang.push(animals)
-    } else if (animals[i][0] !== kandang[i][0]) {
-      var newkandang = []
-      newkandang.push(animals[i])
+  var temp = []
+  var counter = 0
+  animals.sort()
+  for (var i = 0; i < animals.length; i++) {
+    if (temp.length === 0) {
+      temp.push([animals[i]])
+    } else if (temp[counter][0][0] === animals[i][0]) {
+      temp[counter].push(animals[i])
+    } else {
+      temp.push([animals[i]])
+      counter += 1
     }
   }
-  result.push(kandang, newkandang)
-  //console.log(animals[0][0])
-  return result
+  return temp
 }
 
 // TEST CASES
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']))
 // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]))
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak']))
 // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
