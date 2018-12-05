ae5de5ad821706aa95f9eb269794e77ed137db26
Amirul Wiramuda <amirulwiramuda@gmail.com>
2018-12-05 21:41:51 +0700

    update

--- pengelompokanhewan.js
+++ pengelompokanhewan.js
@@ -19,5 +19,7 @@ function groupAnimals (animals) {
 // TEST CASES
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']))
 // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak']))
+console.log(
+  groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak'])
+)
 // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
